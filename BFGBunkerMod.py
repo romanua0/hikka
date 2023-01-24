058e46afb443944969ba033133b8666472c940d1
romanua0 <107593019+romanua0@users.noreply.github.com>
2023-01-24 20:56:18 +0200

    Add files via upload

---
+++ BFGBunkerMod.py
@@ -0,0 +1,44 @@
+# ---------------------------------------------------------------------------------
+#  /\_/\  
+# ( o.o )  🔓 Not licensed.
+#  > ^ <   ⚠️ Owner of heta.hikariatama.ru doesn't take any responsibilities or intellectual property rights regarding this script
+# ---------------------------------------------------------------------------------
+# Name: BFG Bunker Mod
+# Description: 
+# Author: romanua0
+# Commands:
+# .bfgb1on | .bfgb1off
+# ---------------------------------------------------------------------------------
+
+
+
+
+from .. import utils, loader
+from asyncio import sleep
+
+@loader.tds
+class BFGBunkerMod(loader.Module):
+    """Автофарм теплиц для BFG Bunker Bot"""
+
+    strings = {"name": "BFGBunker"}
+
+    async def client_ready(self, client, db):
+        self.db = db
+
+    async def bfgb1oncmd(self, message):
+        """Insert info about command here... """
+        await utils.answer(message, "<b>Запущене выращивание картошки</b>")
+        status = self.db.set("BFGBunker", "status1", True)
+        while status:
+            for i in range(15):
+                if not self.db.get("BFGBunker", "status1"):
+                    return
+                await message.respond("вырастить картошку")
+                await sleep(600)
+            status = self.db.get("BFGBunker", "status1")
+            await message.respond("вырастить картошку")
+
+    async def bfgb1offcmd(self, message):
+        """Insert info about command here... """
+        self.db.set("BFGBunker", "status1", False)
+        await utils.answer(message, "<b>Остановлено выращивание картошки</b>")
