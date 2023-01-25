666d5aeb74c2791e170998e75ba4f756cc183180
romanua0 <107593019+romanua0@users.noreply.github.com>
2023-01-25 16:10:23 +0200

    Update MineEVO.py

--- MineEVO.py
+++ MineEVO.py
@@ -34,7 +34,7 @@ class MineEVOMod(loader.Module):
                 if not self.db.get("MineEVO", "status1"):
                     return
                 await message.respond("коп")
-                await sleep(2)
+                await sleep(4)
             status = self.db.get("MineEVO", "status1")
             await message.respond("Профиль")
 
@@ -43,20 +43,3 @@ class MineEVOMod(loader.Module):
         self.db.set("MineEVO", "status1", False)
         await utils.answer(message, "<b>Остановлено</b>")
         
-            async def minee4oncmd(self, message):
-        """Insert info about command here... """
-        await utils.answer(message, "<b>Запущено 4с</b>")
-        status = self.db.set("MineEVO", "status1", True)
-        while status:
-            for i in range(15):
-                if not self.db.get("MineEVO", "status1"):
-                    return
-                await message.respond("Копать")
-                await sleep(4)
-            status = self.db.get("MineEVO", "status1")
-            await message.respond("Проф")
-
-    async def minee4offcmd(self, message):
-        """Insert info about command here... """
-        self.db.set("MineEVO", "status1", False)
-        await utils.answer(message, "<b>Остановлено 4с</b>")
